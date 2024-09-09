13bdc57786136cc7fa07c07cf13b474b8db62352
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-09-10 00:23:22 +0300

    WIP

---
+++ products.js
@@ -0,0 +1,16 @@
+initStore([
+    {
+        id: 0,
+        title: 'Women Hot Collection',
+        description: 'First product description',
+        price: 29,
+        image: 'https://via.placeholder.com/550x750',
+    },
+    {
+        id: 1,
+        title: 'Awesome Pink Show',
+        description: 'Second product description',
+        price: 100,
+        image: 'https://via.placeholder.com/550x750',
+    },
+]);

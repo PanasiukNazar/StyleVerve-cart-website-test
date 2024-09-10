555493b074c67f22d60717710c150fa52e56a016
NazarPanasiuk <nazar.panasiuk.ua@gmail.com>
2024-09-10 07:31:32 +0300

    WIP

--- products.js
+++ products.js
@@ -7,10 +7,31 @@ initStore([
         image: 'https://via.placeholder.com/550x750',
     },
     {
-        id: 1,
+        id: 2,
         title: 'Awesome Pink Show',
         description: 'Second product description',
         price: 100,
+        image: 'https://via.placeholder.com/550x750',
+    },
+    {
+        id: 3,
+        title: 'Great Red Shoes',
+        description: 'third product description',
+        price: 1100,
+        image: 'https://via.placeholder.com/550x750',
+    },
+    {
+        id: 4,
+        title: 'Nice Purple Jacket',
+        description: 'forth product description',
+        price: 75,
+        image: 'https://via.placeholder.com/550x750',
+    },
+    {
+        id: 5,
+        title: 'Awesome Green Shorts',
+        description: 'fifth product description',
+        price: 30,
         image: 'https://via.placeholder.com/550x750',
     },
 ]);
